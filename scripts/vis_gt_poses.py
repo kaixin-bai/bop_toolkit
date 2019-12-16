c0eb86f86bb294d92ceb22a69f0774ee167a5b8e
kaixin-bai <38804949+kaixin-bai@users.noreply.github.com>
2019-12-16 22:24:47 +0100

    Update vis_gt_poses.py

--- scripts/vis_gt_poses.py
+++ scripts/vis_gt_poses.py
@@ -18,7 +18,7 @@ from bop_toolkit_lib import visualization
 ################################################################################
 p = {
   # See dataset_params.py for options.
-  'dataset': 'lm',
+  'dataset': 'ycbv',
 
   # Dataset split. Options: 'train', 'val', 'test'.
   'dataset_split': 'test',
